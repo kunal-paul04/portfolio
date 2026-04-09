0d7256db4044f280dc7323a624e4756f1714d5aa
kunal-paul04 <kunalkantipaul@gmail.com>
2026-04-09 14:32:40 +0530

    feat: deployment in vercel

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
@@ -7,20 +8,22 @@ from app.api.routes import auth
 
 app = FastAPI()
 
+# Base path fix (VERY IMPORTANT for Vercel)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 # Static & Templates
-app.mount("/static", StaticFiles(directory="app/static"), name="static")
-templates = Jinja2Templates(directory="app/templates")
+app.mount("/static", StaticFiles(directory=os.path.join(BASE_DIR, "app/static")), name="static")
+templates = Jinja2Templates(directory=os.path.join(BASE_DIR, "templates"))
 
 # Include Routes
 app.include_router(auth.router, prefix="/api/auth", tags=["auth"])
 
-
-# Page Routes
+# Page Route
 @app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
-    return templates.TemplateResponse(request, "index.html")
+    return templates.TemplateResponse("index.html", {"request": request})
 
 
-@app.get("/admin", response_class=HTMLResponse)
-async def admin_page(request: Request):
-    return templates.TemplateResponse(request, "admin.html")
+# @app.get("/admin", response_class=HTMLResponse)
+# async def admin_page(request: Request):
+#     return templates.TemplateResponse(request, "admin.html")
